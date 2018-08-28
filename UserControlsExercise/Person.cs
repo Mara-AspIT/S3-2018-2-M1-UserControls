f72046a5247ed7ff8e55c24852f1ce4603b02f50
Mads Mikkel Rasmussen <mara@aspit.dk>
2018-08-28 19:59:42 +0200

    Add project files.

---
+++ UserControlsExercise/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserControlsExercise
+{
+    public class Person
+    {
+        public string Firstname
+        {
+            get;set;
+        }
+
+    }
+}
---
+++ UserControlsExercise/PersonRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserControlsExercise
+{
+    public static class PersonRepository
+    {
+        static List<Person> people = new List<Person>();
+
+        public static List<Person> GetAllPersons()
+        {
+            return people;
+            ;
+        }
+
+        public static void SaveNew(Person p)
+        {
+            people.Add(p);
+        }
+
+    }
+}
